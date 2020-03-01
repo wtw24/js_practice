f5e7f2f1cfda3553b0f20bf8b6cd0c1c19a85388
Wladimir Wendland <wtw24.com@gmail.com>
2020-03-01 11:12:54 +0100

    simple .popup_engineer Modal Window

---
+++ src/js/modules/modals.js
@@ -0,0 +1,27 @@
+const modals = () => {
+    function bindModal(trigger, modal, close) {
+        trigger.addEventListener('click', (e) => {
+            console.log('click1');
+
+            if (e.target) {
+                e.preventDefault();
+            }
+
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+    }
+
+    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+        modalEngineer = document.querySelector('.popup_engineer'),
+        modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+};
+
+export default modals;
